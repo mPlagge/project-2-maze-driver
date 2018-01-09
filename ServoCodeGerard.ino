955140ebe77886b93fdbeed12f9ea1036ecf656b
merijn <0941200@hr.nl>
2018-01-09 13:15:17 +0100

    Code van de servo's voor het voertuig van Gerard toegevoegd

---
+++ ServoCodeGerard.ino
@@ -0,0 +1,63 @@
+// library voor servo's
+#include <Servo.h>
+
+// Servo pin's
+#define servoPinLeft 12
+#define servoPinRight 14
+
+// Create objects for the weels
+Servo ServoLeft;
+Servo ServoRight;
+
+/*************************************************************************/
+
+void setup() {
+    // Serial stuff
+    Serial.begin(9600);
+
+    // Bind servo objects to pins
+    ServoRight.attach(servoPinRight);
+    ServoLeft.attach(servoPinLeft);
+}
+
+/*************************************************************************/
+
+
+void loop() {
+  // ga naar beneden 100 mili seconden
+  weelsDowns(100)
+}
+
+/*************************************************************************/
+
+// void's with a timer you can set
+
+void weelsDowns(float timelapse) {
+  Serial.println("weels up signal");
+  ServoLeft.write(160);
+  ServoRight.write(-160);
+  delay(timelapse);
+}
+
+void weelsStop(float timelapse) {
+  Serial.println("weels stop signal");
+  ServoLeft.write(90);
+  ServoRight.write(90);
+  delay(timelapse);
+}
+
+void weelsUp(float timelapse) {
+  Serial.println("weels up signal");
+  ServoLeft.write(-160);
+  ServoRight.write(160);
+  delay(timelapse);
+}
+
+void weelsTurnLeft(float timelapse) {
+  Serial.println("Weels Turn Left");
+  ServoLeft.write(90);
+  ServoRight.write(360);
+  delay(timelapse);
+}
+
+
